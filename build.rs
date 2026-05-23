9bdc9757348ededa6161551f276becdf8b79160f
Nikita Zavartsev <nikoss9644@gmail.com>
2026-05-23 20:19:18 +0200

    ci: [release-workflow] add standard packaging for all target OS

--- build.rs
+++ build.rs
@@ -6,5 +6,9 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
         println!("cargo:rustc-link-arg=/EXPORT:NvOptimusEnablement");
         println!("cargo:rustc-link-arg=/EXPORT:AmdPowerXpressRequestHighPerformance");
+        let mut res = winres::WindowsResource::new();
+        // Point this to your actual .ico file
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
     }
 }
